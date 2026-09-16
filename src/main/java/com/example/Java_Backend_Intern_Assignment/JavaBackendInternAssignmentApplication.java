558aee6f0ce28f4555ee34a7250cdf3c0edeb407
Rishikesh Srivastava <srishikesh343@gmail.com>
2026-09-16 15:23:50 +0530

    Initial Spring Boot Project setup

---
+++ src/main/java/com/example/Java_Backend_Intern_Assignment/JavaBackendInternAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_Backend_Intern_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendInternAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendInternAssignmentApplication.class, args);
+	}
+
+}
